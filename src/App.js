ab0e63ecbe6cba263e64e6ae7f38d5ff06e2a738
Kashyap297 <kashyap29700@gmail.com>
2023-11-23 17:04:50 +0530

    counter deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-// import Card from './cardpr1/Card';
-import Counter from './counterpr2/counter';
+import Card from './cardpr1/Card';
+
 // import Card from './cardpr1/Card';
 
 
 function App() {
   return (
-    <Counter />
+    <Card />
   );
 }
 
--- src/counterpr2/counter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Counter = () => {
-  return (
-    <div>My name os lujan</div>
-  )
-}
-
-export default Counter
